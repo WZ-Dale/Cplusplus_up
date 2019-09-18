08be8586e02b7a0cf2b6214a44604592f38af395
wangze <w18829898295@163.com>
2019-09-18 11:23:04 +0800

    编程练习：密码等级_ok

--- GetPwdSecurityLevel/GetPwdSecurityLevel/main.cpp
+++ GetPwdSecurityLevel/GetPwdSecurityLevel/main.cpp
@@ -2,26 +2,18 @@
 #include <string>
 
 using namespace std;
+
 class GetPwdSecurityLevel
 {
 public:
-	GetPwdSecurityLevel(string password_str) :_score(0), _password_str(password_str){}
-	int size_score(){
-		if (_password_str.size() >= 0 && _password_str.size() <= 4){
-			return 5;
-		}
-		else if (_password_str.size() >= 5 && _password_str.size() <= 7){
-			return 10;
-		}
-		else if (_password_str.size() >= 8){
-			return 25;
-		}
-	}
-	int letter_score(){
-		int a_letter_num = 0;
-		int A_letter_num = 0;
+	GetPwdSecurityLevel(string password_str) 
+		:_password_str(password_str), _size(password_str.size()), _score(0) {}
+	
+	void statistics(){
+		int a_letter_num = 0, A_letter_num = 0;
 		int number_num = 0;
 		int symbol_num = 0;
+		//统计出现次数
 		for (auto& e : _password_str){
 			if (e >= 'a' && e <= 'z'){
 				++a_letter_num;
@@ -32,17 +24,55 @@ public:
 			else if (e >= '0' && e <= '9'){
 				++number_num;
 			}
-			else{
+			else {
 				++symbol_num;
 			}
 		}
-		if ((a_letter_num == _password_str.size() || A_letter_num == _password_str.size())){
-
+		//规则一
+		if (_size > 0 && _size <= 4){
+			_score += 5;
 		}
-
+		else if (_size >= 5 && _size <= 7){
+			_score += 10;
+		}
+		else if (_size >= 8){
+			_score += 25;
+		}
+		//规则二
+		if ((a_letter_num == _size || A_letter_num == _size)){//全都是小（大）写字母
+			_score += 10;
+		}
+		else if (a_letter_num > 0 && A_letter_num > 0){//大小写混合字母
+			_score += 20;
+		}
+		//规则三
+		if (number_num == 1){//1个数字
+			_score += 10;
+		}
+		else if (number_num > 1){//大于1个数字
+			_score += 20;
+		}
+		//规则四
+		if (symbol_num == 1){//1个符号
+			_score += 10;
+		}
+		else if (symbol_num > 1){//大于1个符号
+			_score += 25;
+		}
+		//规则五
+		if (a_letter_num + A_letter_num + number_num == _size){//字母和数字
+			_score += 2;
+		}
+		else if (a_letter_num + A_letter_num + number_num + symbol_num == _size){//字母、数字和符号
+			_score += 3;
+		}
+		else if (a_letter_num > 0 && A_letter_num > 0 && a_letter_num + A_letter_num + number_num + symbol_num == _size){//大小写字母、数字和符号
+			_score += 5;
+		}
 	}
 	string GetLevel(){
-		if (_score >= 0 && _score < 25){
+		statistics();
+		if (_size == 0 || _score >= 0 && _score < 25){
 			return "VERY_WEAK";
 		}
 		else if (_score >= 25 && _score < 50){
@@ -66,8 +96,9 @@ public:
 	}
 
 public:
-	int _score;
 	string _password_str;
+	int _size;
+	int _score;
 };
 
 int main(){
